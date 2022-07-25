a2f1d99a5d140048607e1144af7f33fe11cf1151
Beliathal <19905041+Beliathal@users.noreply.github.com>
2022-07-25 13:33:08 +0300

    Add files via upload

--- QFIL Helper/My Project/AssemblyInfo.vb
+++ QFIL Helper/My Project/AssemblyInfo.vb
@@ -31,6 +31,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.541")> 
-<Assembly: AssemblyFileVersion("1.0.0.541")> <Assembly: AssemblyInformationalVersion("0.0.0.0")>
+<Assembly: AssemblyVersion("1.0.0.569")> 
+<Assembly: AssemblyFileVersion("1.0.0.569")> <Assembly: AssemblyInformationalVersion("0.0.0.0")>
 
